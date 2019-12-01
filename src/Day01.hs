b8a08fa9e8b9b8b9c091baa17a816f288b630c95
Steve Purcell <steve@sanityinc.com>
2019-12-01 20:54:46 +1300

    Day 1b

--- src/Day01.hs
+++ src/Day01.hs
@@ -7,13 +7,21 @@ import Parse (Parser)
 fuelRequired :: Int -> Int
 fuelRequired mass = mass `div` 3 - 2
 
+fullFuelRequired = sum . takeWhile (>= 0) . tail . iterate fuelRequired
+
+day1a = sum . fmap fuelRequired
+
+day1b = sum . fmap fullFuelRequired
 
 numbers :: Parser [Int]
 numbers = P.many (P.decimal <* P.newline)
 
-
-
-day1 :: IO Int
+day1 :: IO ()
 day1 = do
+  putStrLn "Day 1"
   vals <- P.parseFile numbers "input/01.txt"
-  pure $ sum $ fuelRequired <$> vals
+  putStrLn "A:"
+  print (day1a vals)
+  putStrLn "B:"
+  print (day1b vals)
+
